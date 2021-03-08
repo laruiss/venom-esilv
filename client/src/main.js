36e5922db9a7ded14d4038e9faf562fb8ea4f1a5
Stanislas Ormières <stan@stormier.ninja>
2021-03-08 15:54:02 +0100

    :sparkles: Add Font-Awesome

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { FontAwesomeIcon } from './plugins/font-awesome'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .component('fa-icon', FontAwesomeIcon)
+  .mount('#app')
