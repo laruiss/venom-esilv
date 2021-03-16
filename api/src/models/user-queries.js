c59c650bdebbfca2a7406c74cf52a86fcdec3fdc
Stanislas Ormières <stan@stormier.ninja>
2021-03-16 16:37:34 +0100

    :sparkles: Add jsdoc for UserMongooseDocument

--- api/src/models/user-queries.js
+++ api/src/models/user-queries.js
@@ -1,8 +1,35 @@
 import User from './user-model.js'
 
+// CRUD
+// Create
+// Retrieve
+// Update
+// Delete
+
+/**
+ * Crée un utilisateur dans la base de données
+ * 
+ * @function
+ * @async
+ * 
+ * @param {import('./user-model').UserData} userData 
+ * @returns {Promise.<import('./user-model').UserMongooseDocument>}
+ */
 export function createUser (userData) {
   const user = new User(userData)
   return user.save().then(() => user)
+}
+
+export function getUserById(id) {
+  return User.findById(id)
+}
+
+export function getUserByLogin(login) {
+  return User.findOne({ login })
+}
+
+export function getUsers () {
+  return User.find()
 }
 
 export function modifyUser (userData) {
@@ -10,4 +37,8 @@ export function modifyUser (userData) {
     .then(user => {
       // (...)
     })
+}
+
+export function deleteUser (user) {
+  User.deleteOne({ id: user.id })
 }
